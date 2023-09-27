5a653425752297bf55a21dd8c429df12d5417b63
Jenny Wadkins <threnjen@gmail.com>
2023-09-26 19:31:59 -0700

    remove arg parser for readability

---
+++ app/arguments.py
@@ -0,0 +1,32 @@
+import random
+
+INPUT_CONFIG = {
+    "reset": [False, bool, "Start retraining the model from scratch"],
+    "opponent_type": [
+        "mostly_best",
+        str,
+        "best / mostly_best / random / base / rules - the type of opponent to train against",
+    ],
+    "debug": [False, bool, "Debug logging"],
+    "verbose": [False, bool, "Show observation in debug output"],
+    "rules": [False, bool, "Evaluate on a ruled-based agent"],
+    "best": [False, bool, "Uses best moves when evaluating agent against rules-based agent"],
+    "env_name": [
+        "tictactoe",
+        str,
+        "Which gym environment to train in: tictactoe, connect4, sushigo, butterfly, geschenkt, frouge",
+    ],
+    "seed": [random.randint(0, 1000), int, "Random seed"],
+    "eval_freq": [10240, int, "How many timesteps should each actor contribute before the agent is evaluated?"],
+    "n_eval_episodes": [100, int, "How many episodes should each actor contirbute to the evaluation of the agent"],
+    "threshold": [0.2, float, "What score must the agent achieve during evaluation to 'beat' the previous version?"],
+    "gamma": [0.99, float, "The value of gamma in PPO"],
+    "timesteps_per_actorbatch": [1024, int, "How many timesteps should each actor contribute to the batch?"],
+    "clip_param": [0.2, float, "The clip paramater in PPO"],
+    "entcoeff": [0.1, float, "The entropy coefficient in PPO"],
+    "optim_epochs": [4, int, "The number of epoch to train the PPO agent per batch"],
+    "optim_stepsize": [0.0003, float, "The step size for the PPO optimiser"],
+    "optim_batchsize": [1024, int, "The minibatch size in the PPO optimiser"],
+    "lam": [0.95, float, "The value of lambda in PPO"],
+    "adam_epsilon": [1e-05, float, "The value of epsilon in the Adam optimiser"],
+}
--- app/train.py
+++ app/train.py
@@ -14,6 +14,7 @@ import argparse
 import time
 from shutil import copyfile
 from mpi4py import MPI
+from arguments import INPUT_CONFIG
 
 from stable_baselines.ppo1 import PPO1
 from stable_baselines.common.callbacks import EvalCallback
@@ -29,11 +30,32 @@ from utils.selfplay import selfplay_wrapper
 
 import config
 
+model_reset = INPUT_CONFIG["reset"][0]
+opponent_type = INPUT_CONFIG["opponent_type"][0]
+debug_logging = INPUT_CONFIG["debug"][0]
+verbose_output = INPUT_CONFIG["verbose"][0]
+use_rules = INPUT_CONFIG["rules"][0]
+use_best = INPUT_CONFIG["best"][0]
+env_name = INPUT_CONFIG["env_name"][0]
+random_seed = INPUT_CONFIG["seed"][0]
+eval_freq = INPUT_CONFIG["eval_freq"][0]
+n_eval_episodes = INPUT_CONFIG["n_eval_episodes"][0]
+threshold = INPUT_CONFIG["threshold"][0]
+gamma = INPUT_CONFIG["gamma"][0]
+timesteps_per_actorbatch = INPUT_CONFIG["timesteps_per_actorbatch"][0]
+clip_param = INPUT_CONFIG["clip_param"][0]
+entcoeff = INPUT_CONFIG["entcoeff"][0]
+optim_epochs = INPUT_CONFIG["optim_epochs"][0]
+optim_stepsize = INPUT_CONFIG["optim_stepsize"][0]
+optim_batchsize = INPUT_CONFIG["optim_batchsize"][0]
+lam = INPUT_CONFIG["lam"][0]
+adam_epsilon = INPUT_CONFIG["adam_epsilon"][0]
 
-def main(args):
+
+def main(arg_config):
     rank = MPI.COMM_WORLD.Get_rank()
 
-    model_dir = os.path.join(config.MODELDIR, args.env_name)
+    model_dir = os.path.join(config.MODELDIR, env_name)
 
     if rank == 0:
         try:
@@ -41,38 +63,38 @@ def main(args):
         except:
             pass
         reset_logs(model_dir)
-        if args.reset:
+        if model_reset:
             reset_models(model_dir)
         logger.configure(config.LOGDIR)
     else:
         logger.configure(format_strs=[])
 
-    if args.debug:
+    if debug_logging:
         logger.set_level(config.DEBUG)
     else:
         time.sleep(5)
         logger.set_level(config.INFO)
 
-    workerseed = args.seed + 10000 * MPI.COMM_WORLD.Get_rank()
+    workerseed = random_seed + 10000 * MPI.COMM_WORLD.Get_rank()
     set_global_seeds(workerseed)
 
     logger.info("\nSetting up the selfplay training environment opponents...")
-    base_env = get_environment(args.env_name)
-    env = selfplay_wrapper(base_env)(opponent_type=args.opponent_type, verbose=args.verbose)
+    base_env = get_environment(env_name)
+    env = selfplay_wrapper(base_env)(opponent_type=opponent_type, verbose=verbose_output)
     env.seed(workerseed)
 
-    CustomPolicy = get_network_arch(args.env_name)
+    CustomPolicy = get_network_arch(env_name)
 
     params = {
-        "gamma": args.gamma,
-        "timesteps_per_actorbatch": args.timesteps_per_actorbatch,
-        "clip_param": args.clip_param,
-        "entcoeff": args.entcoeff,
-        "optim_epochs": args.optim_epochs,
-        "optim_stepsize": args.optim_stepsize,
-        "optim_batchsize": args.optim_batchsize,
-        "lam": args.lam,
-        "adam_epsilon": args.adam_epsilon,
+        "gamma": gamma,
+        "timesteps_per_actorbatch": timesteps_per_actorbatch,
+        "clip_param": clip_param,
+        "entcoeff": entcoeff,
+        "optim_epochs": optim_epochs,
+        "optim_stepsize": optim_stepsize,
+        "optim_batchsize": optim_batchsize,
+        "lam": lam,
+        "adam_epsilon": adam_epsilon,
         "schedule": "linear",
         "verbose": 1,
         "tensorboard_log": config.LOGDIR,
@@ -80,7 +102,7 @@ def main(args):
 
     time.sleep(5)  # allow time for the base model to be saved out when the environment is created
 
-    if args.reset or not os.path.exists(os.path.join(model_dir, "best_model.zip")):
+    if model_reset or not os.path.exists(os.path.join(model_dir, "best_model.zip")):
         logger.info("\nLoading the base PPO agent to train...")
         model = PPO1.load(os.path.join(model_dir, "base.zip"), env, **params)
     else:
@@ -90,31 +112,31 @@ def main(args):
     # Callbacks
     logger.info("\nSetting up the selfplay evaluation environment opponents...")
     callback_args = {
-        "eval_env": selfplay_wrapper(base_env)(opponent_type=args.opponent_type, verbose=args.verbose),
+        "eval_env": selfplay_wrapper(base_env)(opponent_type=opponent_type, verbose=verbose_output),
         "best_model_save_path": config.TMPMODELDIR,
         "log_path": config.LOGDIR,
-        "eval_freq": args.eval_freq,
-        "n_eval_episodes": args.n_eval_episodes,
+        "eval_freq": eval_freq,
+        "n_eval_episodes": n_eval_episodes,
         "deterministic": False,
         "render": True,
         "verbose": 0,
     }
 
-    if args.rules:
+    if use_rules:
         logger.info("\nSetting up the evaluation environment against the rules-based agent...")
         # Evaluate against a 'rules' agent as well
         eval_actual_callback = EvalCallback(
-            eval_env=selfplay_wrapper(base_env)(opponent_type="rules", verbose=args.verbose),
+            eval_env=selfplay_wrapper(base_env)(opponent_type="rules", verbose=verbose_output),
             eval_freq=1,
-            n_eval_episodes=args.n_eval_episodes,
-            deterministic=args.best,
+            n_eval_episodes=n_eval_episodes,
+            deterministic=use_best,
             render=True,
             verbose=0,
         )
         callback_args["callback_on_new_best"] = eval_actual_callback
 
     # Evaluate the agent against previous versions
-    eval_callback = SelfPlayCallback(args.opponent_type, args.threshold, args.env_name, **callback_args)
+    eval_callback = SelfPlayCallback(opponent_type, threshold, env_name, **callback_args)
 
     logger.info("\nSetup complete - commencing learning...\n")
 
@@ -124,98 +146,5 @@ def main(args):
     del env
 
 
-def cli() -> None:
-    """Handles argument extraction from CLI and passing to main().
-    Note that a separate function is used rather than in __name__ == '__main__'
-    to allow unit testing of cli().
-    """
-    # Setup argparse to show defaults on help
-    formatter_class = argparse.ArgumentDefaultsHelpFormatter
-    parser = argparse.ArgumentParser(formatter_class=formatter_class)
-
-    parser.add_argument(
-        "--reset", "-r", action="store_true", default=False, help="Start retraining the model from scratch"
-    )
-    parser.add_argument(
-        "--opponent_type",
-        "-o",
-        type=str,
-        default="mostly_best",
-        help="best / mostly_best / random / base / rules - the type of opponent to train against",
-    )
-    parser.add_argument("--debug", "-d", action="store_true", default=False, help="Debug logging")
-    parser.add_argument("--verbose", "-v", action="store_true", default=False, help="Show observation in debug output")
-    parser.add_argument("--rules", "-ru", action="store_true", default=False, help="Evaluate on a ruled-based agent")
-    parser.add_argument(
-        "--best",
-        "-b",
-        action="store_true",
-        default=False,
-        help="Uses best moves when evaluating agent against rules-based agent",
-    )
-    parser.add_argument(
-        "--env_name",
-        "-e",
-        type=str,
-        default="tictactoe",
-        help="Which gym environment to train in: tictactoe, connect4, sushigo, butterfly, geschenkt, frouge",
-    )
-    parser.add_argument("--seed", "-s", type=int, default=17, help="Random seed")
-
-    parser.add_argument(
-        "--eval_freq",
-        "-ef",
-        type=int,
-        default=10240,
-        help="How many timesteps should each actor contribute before the agent is evaluated?",
-    )
-    parser.add_argument(
-        "--n_eval_episodes",
-        "-ne",
-        type=int,
-        default=100,
-        help="How many episodes should each actor contirbute to the evaluation of the agent",
-    )
-    parser.add_argument(
-        "--threshold",
-        "-t",
-        type=float,
-        default=0.2,
-        help="What score must the agent achieve during evaluation to 'beat' the previous version?",
-    )
-    parser.add_argument("--gamma", "-g", type=float, default=0.99, help="The value of gamma in PPO")
-    parser.add_argument(
-        "--timesteps_per_actorbatch",
-        "-tpa",
-        type=int,
-        default=1024,
-        help="How many timesteps should each actor contribute to the batch?",
-    )
-    parser.add_argument("--clip_param", "-c", type=float, default=0.2, help="The clip paramater in PPO")
-    parser.add_argument("--entcoeff", "-ent", type=float, default=0.1, help="The entropy coefficient in PPO")
-
-    parser.add_argument(
-        "--optim_epochs", "-oe", type=int, default=4, help="The number of epoch to train the PPO agent per batch"
-    )
-    parser.add_argument(
-        "--optim_stepsize", "-os", type=float, default=0.0003, help="The step size for the PPO optimiser"
-    )
-    parser.add_argument(
-        "--optim_batchsize", "-ob", type=int, default=1024, help="The minibatch size in the PPO optimiser"
-    )
-
-    parser.add_argument("--lam", "-l", type=float, default=0.95, help="The value of lambda in PPO")
-    parser.add_argument(
-        "--adam_epsilon", "-a", type=float, default=1e-05, help="The value of epsilon in the Adam optimiser"
-    )
-
-    # Extract args
-    args = parser.parse_args()
-
-    # Enter main
-    main(args)
-    return
-
-
 if __name__ == "__main__":
-    cli()
+    main(INPUT_CONFIG)
